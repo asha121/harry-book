9910889d39c405673393879a62c08c7ba532f820
Test developer <test@testmail.com>
2016-12-15 12:18:18 +0530

    REFACTOR >> code

--- harry/src/main/java/com/clean/books/harry/BookBasket.java
+++ harry/src/main/java/com/clean/books/harry/BookBasket.java
@@ -3,11 +3,13 @@ package com.clean.books.harry;
 public class BookBasket {
 
 	public double getPrice(int books) {
-		if (books == 1) {
-			return 8.0;
-		}
-		return 8.0 * 0.95 * books;
+		double price;
+		if (books == 1)
+			price = 8.0;
+		else
+			price = 8.0 * 0.95 * books;
 
+		return price;
 	}
 
 }

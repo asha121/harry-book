4f6f1530c2b56a23501923baac077c7b37cf5c32
Test developer <test@testmail.com>
2016-12-15 11:51:29 +0530

    RED >> failing test case for 2 books

--- harry/src/test/java/com/clean/books/harry/BookBasketShould.java
+++ harry/src/test/java/com/clean/books/harry/BookBasketShould.java
@@ -10,4 +10,10 @@ public class BookBasketShould {
 	public void return_single_book_price() {
 		assertEquals(8, new BookBasket().getPrice());
 	}
+
+	@Test
+	public void return_two_different_title_book_price() {
+		BookBasket basket = new BookBasket();
+		assertEquals(16 * 0.95, basket.getPrice());
+	}
 }

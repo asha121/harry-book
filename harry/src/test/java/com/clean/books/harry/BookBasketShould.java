794d6d6dadb8de057fd9b9f84ddd431afba747b5
Test developer <test@testmail.com>
2016-12-15 12:25:32 +0530

    RED >  new trest case added

--- harry/src/test/java/com/clean/books/harry/BookBasketShould.java
+++ harry/src/test/java/com/clean/books/harry/BookBasketShould.java
@@ -10,7 +10,7 @@ public class BookBasketShould {
 	public void return_single_book_price() {
 		double actual = new BookBasket().getPrice(1);
 		double expected = 8;
-		double delta = actual - expected;
+		double delta = 0;
 		assertEquals(expected, actual, delta);
 	}
 
@@ -18,7 +18,14 @@ public class BookBasketShould {
 	public void return_two_different_title_book_price() {
 		double actual = new BookBasket().getPrice(2);
 		double expected = 15.2;
-		double delta = actual - expected;
+		double delta = 0;
+		assertEquals(expected, actual, delta);
+	}
+	@Test
+	public void return_three_different_title_book_price() {
+		double actual = new BookBasket().getPrice(3);
+		double expected = 8.0*0.90*3;
+		double delta =0;
 		assertEquals(expected, actual, delta);
 	}
 }
